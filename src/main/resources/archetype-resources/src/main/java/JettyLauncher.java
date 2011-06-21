b8569911b600f4f6dd0cd3740f2f07f9d3995562
John Simone <jsimone@salesforce.com>
2011-06-21 12:40:43 -0700

    remove db.com sdk dependencies

---
+++ src/main/resources/archetype-resources/src/main/java/JettyLauncher.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.StringUtil;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * 
+ * This class launches the web application in an embedded Jetty container.
+ * This is the entry point to your application. The Java command that is used for
+ * launching should fire this main method.
+ *
+ * @author John Simone
+ */
+public class JettyLauncher {
+    
+    private URL getArtifactLocation() {
+        return this.getClass().getProtectionDomain().getCodeSource().getLocation();
+    }
+    
+    private static void startJetty(String webappDirLocation, String classpath, int port) throws Exception {
+        Server server = new Server(port);
+        WebAppContext root = new WebAppContext();
+
+        root.setContextPath("/");
+        root.setDescriptor(webappDirLocation+"/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+
+        //Jetty expects the classpath to be semi-colon 
+        classpath = StringUtil.replace(classpath, " ", ";");
+        
+        root.setExtraClasspath(classpath);            
+        
+        //Parent loader priority is a class loader setting that Jetty accepts.
+        //By default Jetty will behave like most web containers in that it will
+        //allow your application to replace non-server libraries that are part of the
+        //container. Setting parent loader priority to true changes this behavior.
+        //Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        root.setParentLoaderPriority(true);
+        
+        server.setHandler(root);
+ 
+        server.start();
+        server.join();        
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        String webappDirLocation = null;
+        if(args.length<1) {
+            System.out.println("Missing argument: path_to_webapp_directory. Checking default location: 'src/main/webapp/'");
+            webappDirLocation = "src/main/webapp/";
+        } else {
+            webappDirLocation = args[0];
+        }
+        
+        File webAppDir = new File(webappDirLocation);
+        if(!webAppDir.exists() || !webAppDir.isDirectory()) {
+            System.out.println("Error locating the webapp directory specified: " + webappDirLocation);
+            System.exit(1);
+        }
+
+        //Retrieve the classpath value from the manifest of the archive
+        //this will be used to add to the classpath of the webapp. This replaces
+        //the usual process of storing jars in the WEB-INF/lib and allows them to
+        //be accessed externally
+        JettyLauncher jettyLauncher = new JettyLauncher();
+        JarFile jarFile = new JarFile(new File(jettyLauncher.getArtifactLocation().getPath()));
+        Attributes attributes = jarFile.getManifest().getMainAttributes();
+        String classpath = attributes.getValue("Class-Path");
+        
+        //Heroku will set the port that we should run on into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        //Start the jetty server
+        startJetty(webappDirLocation, classpath, Integer.valueOf(webPort));
+    }
+
+}
--- src/main/resources/archetype-resources/src/main/webapp/WEB-INF/tags/template.tag
+++ src/main/resources/archetype-resources/src/main/webapp/WEB-INF/tags/template.tag
@@ -8,7 +8,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<title>Database.com App: ${artifactId}</title>
+	<title>Java on Heroku Sample: ${artifactId}</title>
 	<link href="${symbol_dollar}{pageContext.request.contextPath}/resources/layout.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
